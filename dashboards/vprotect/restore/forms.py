04a3e224ebe658c8b1a0f9a766d7bec708b27626
Paweł Kiersznowski <pawel@kiersznowski.com>
2020-03-09 14:15:51 +0100

    added restore storage id field

--- dashboards/vprotect/restore/forms.py
+++ dashboards/vprotect/restore/forms.py
@@ -9,6 +9,7 @@ from horizon import exceptions
 class Restore(forms.SelfHandlingForm):
     backup = forms.ChoiceField(label=_("Backup"), required = True)
     restored_name = forms.CharField(label=_("Virtual machine name (optional)"), required = False)
+    restore_storage_id = forms.CharField(label=_("Volume pool (optional)"), required = False)
 
     error_message = _('Couldn\'t restore "%s".')
     success_message = _('Created restore task for instance "%s".')
@@ -22,12 +23,14 @@ class Restore(forms.SelfHandlingForm):
         try:
             backup = data['backup']
             restored_name = data['restored_name']
+            restore_storage_id = data['restore_storage_id']
             vm_guid = self.kwargs['initial']['instance_id']
             hypervisor_manager = utils.fetch_hypervisor_manager(backup)
             payload = { "backup" : backup,
                         "dstProtectedEntity" :  {"guid" : vm_guid, "type" : "VM" },
                         "hypervisorManager": { "guid" : hypervisor_manager[0]['guid'] },
                         "restoredPeName" : restored_name,
+                        "restoreStorageId": restore_storage_id,
                         "tenantId" : self.request.user.tenant_id}
             restore_and_import_task = utils.create_restore_and_import_task(payload)
             messages.success(request, self.success_message % restore_and_import_task.json()['dstProtectedEntity']['name'])
