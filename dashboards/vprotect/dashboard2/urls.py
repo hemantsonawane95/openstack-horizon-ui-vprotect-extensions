213a733239fa08972dc69eabcfe4ac6ea80bcb5c
m.konopko@storware.eu <Michalhehe132@#>
2021-02-12 11:07:13 +0100

    proxy

--- dashboards/vprotect/dashboard2/urls.py
+++ dashboards/vprotect/dashboard2/urls.py
@@ -16,5 +16,7 @@ from openstack_dashboard.dashboards.vprotect.dashboard2 import views
 
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index')
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # url(r'^(?P<schedule_id>[^/]+)/edit-schedule$', views.JsonView.as_view(), name='edit-schedule'),
+    url(r'^json$', views.json, name='json'),
 ]
--- dashboards/vprotect/dashboard2/views.py
+++ dashboards/vprotect/dashboard2/views.py
@@ -1,5 +1,38 @@
 from django.views import generic
+from django.http import JsonResponse
+from django.core import serializers
+import requests
+import yaml
 
+CONFIG = yaml.safe_load(open('/usr/share/openstack-dashboard/openstack_dashboard/dashboards/vprotect/config.yaml', 'r'))
+VPROTECT_API_URL = CONFIG['REST_API_URL']
+USER = CONFIG['USER']
+PASSWORD = CONFIG['PASSWORD']
 
 class IndexView(generic.TemplateView):
     template_name = 'vprotect/dashboard2/index.html'
+
+
+class JsonView(generic.TemplateView):
+    def get(self, request):
+        return JsonResponse(request.body.decode('utf-8'), safe=False)
+
+def login():
+    payload = {
+        "login": CONFIG['USER'],
+        "password": CONFIG['PASSWORD']
+    }
+    headers = {'content-type': 'application/json'}
+    session = requests.Session()
+    session.post(VPROTECT_API_URL + '/session/login', data=json.dumps(payload), headers=headers)
+    return session
+
+def json(request):
+    url = request.build_absolute_uri()
+    if request.method == 'POST':
+        #login().get(VPROTECT_API_URL + ).json()
+        return JsonResponse({url: url})
+    else:
+        return render(request,'accounts/register.html')
+
+    # return JsonResponse(request.decode('utf-8'), safe=False)
