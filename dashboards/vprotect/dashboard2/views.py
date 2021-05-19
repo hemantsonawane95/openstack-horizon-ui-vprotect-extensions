e7dd06d3413893fa8ca803951cd4ba68d651711f
m.konopko@storware.eu <Michalhehe132@#>
2021-05-19 16:11:37 +0200

    download response fix

--- dashboards/vprotect/dashboard2/views.py
+++ dashboards/vprotect/dashboard2/views.py
@@ -1,8 +1,6 @@
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
-import requests
-import yaml
-import json
+import requests, yaml, json
 
 CONFIG = yaml.safe_load(open('/usr/share/openstack-dashboard/openstack_dashboard/dashboards/vprotect/config.yaml', 'r'))
 VPROTECT_API_URL = CONFIG['REST_API_URL']
@@ -26,6 +24,14 @@ def login():
     session = requests.Session()
     session.post(VPROTECT_API_URL + '/session/login', data=json.dumps(payload), headers=headers)
     return session
+
+
+def is_json(myjson):
+  try:
+    json_object = json.dumps(myjson)
+  except ValueError as e:
+    return False
+  return True
 
 def apiProxy(request):
     url = request.build_absolute_uri()
@@ -51,7 +57,13 @@ def apiProxy(request):
     if request.method == "DELETE":
         response = login().delete(path)
 
-    return JsonResponse(response.json(), status=response.status_code, safe=False)
+    if "download" in path:
+        response2 = HttpResponse(response.content)
+        response2['Content-Type'] = response.headers['Content-Type']
+        response2['Content-Disposition'] = response.headers['Content-Disposition']
+        return response2
+    else:
+        return JsonResponse(response.json(), status=response.status_code, safe=False)
 
 def userInfo(request):
     payload = {
