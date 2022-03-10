2b0c44585772f88b4985d66656784c18ca5727b5
kstepien-storware <k.stepien@storware.eu>
2022-03-10 13:27:45 +0100

    Add selector to get released version

--- install.py
+++ install.py
@@ -7,6 +7,8 @@ import yaml
 import requests, zipfile, io
 from distutils.dir_util import copy_tree
 import os
+from pick import pick
+
 
 def update_variable(state, variable):
     with open(CONFIG_PATH) as f:
@@ -16,6 +18,9 @@ def update_variable(state, variable):
 
     with open(CONFIG_PATH, 'w') as f:
         yaml.safe_dump(doc, f, default_flow_style=False)
+
+def getReleaseLabel(release):
+    return release['name'] + " " + release['body']
 
 CONFIG_PATH = 'dashboards/vprotect/config.yaml'
 
@@ -28,16 +33,23 @@ if len(sys.argv) >= 3:
 if len(sys.argv) >= 4:
     update_variable(sys.argv[3], 'PASSWORD')
 
-r = requests.get("https://github.com/Storware/ovirt-engine-ui-vprotect-extensions/releases/download/openstack/openstack.zip")
-z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall("dashboards/vprotect/static/vprotect")
-z.extractall("/usr/share/openstack-dashboard/static/vprotect")
+versions = requests.get("https://api.github.com/repos/Storware/ovirt-engine-ui-vprotect-extensions/releases")
+versionsNames = map(getReleaseLabel, versions.json())
 
-path = '/usr/share/openstack-dashboard/openstack_dashboard/enabled'
-isExist = os.path.exists(path)
-if not isExist:
-    os.makedirs(path)
+option, index = pick(list(versionsNames), "Select a version", indicator='=>')
 
-copy_tree('dashboards/vprotect/', '/usr/share/openstack-dashboard/openstack_dashboard/dashboards/vprotect/')
-shutil.copyfile('enabled/_50_vprotect.py', '/usr/share/openstack-dashboard/openstack_dashboard/enabled/_50_vprotect.py')
+if option:
+    downloadPath = versions.json()[index]['assets'][0]['browser_download_url']
+    r = requests.get("https://github.com/Storware/ovirt-engine-ui-vprotect-extensions/releases/download/openstack/openstack.zip")
 
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("dashboards/vprotect/static/vprotect")
+    z.extractall("/usr/share/openstack-dashboard/static/vprotect")
+
+    path = '/usr/share/openstack-dashboard/openstack_dashboard/enabled'
+    isExist = os.path.exists(path)
+    if not isExist:
+        os.makedirs(path)
+
+    copy_tree('dashboards/vprotect/', '/usr/share/openstack-dashboard/openstack_dashboard/dashboards/vprotect/')
+    shutil.copyfile('enabled/_50_vprotect.py', '/usr/share/openstack-dashboard/openstack_dashboard/enabled/_50_vprotect.py')
