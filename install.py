ebdfc381676513614d95fc01c1c02a60aeeb441b
kstepien-storware <k.stepien@storware.eu>
2022-04-15 15:05:59 +0200

    add new argument to select versions
    you can skip selecting versions in terminal by providing 'latest' or specivied version like '5.0.0-7' in command installation

--- install.py
+++ install.py
@@ -7,7 +7,6 @@ import yaml
 import requests, zipfile, io
 from distutils.dir_util import copy_tree
 import os
-from pick import pick
 
 
 def update_variable(state, variable):
@@ -23,6 +22,8 @@ def getReleaseLabel(release):
     return release['name'] + " " + release['body']
 
 CONFIG_PATH = 'dashboards/vprotect/config.yaml'
+RELEASES_API = 'https://api.github.com/repos/Storware/ovirt-engine-ui-vprotect-extensions/releases'
+VERSION_DATA = None
 
 if len(sys.argv) >= 2:
     update_variable(sys.argv[1], 'REST_API_URL')
@@ -33,16 +34,28 @@ if len(sys.argv) >= 3:
 if len(sys.argv) >= 4:
     update_variable(sys.argv[3], 'PASSWORD')
 
-versions = requests.get("https://api.github.com/repos/Storware/ovirt-engine-ui-vprotect-extensions/releases")
-versionsNames = map(getReleaseLabel, versions.json())
+# If the version of package is provided
+if len(sys.argv) >= 5:
+    requestApi = RELEASES_API + (sys.argv[4] == 'latest' and "/latest" or "/tags/" + sys.argv[4])
 
-option, index = pick(list(versionsNames), "Select a version", indicator='=>')
+    r = requests.get(requestApi)
+    if r.json().get('message'):
+        print(r.json()['message'])
+    else:
+        VERSION_DATA = r.json()
+else:
+    from pick import pick
+    versions = requests.get(RELEASES_API)
+    versionsNames = map(getReleaseLabel, versions.json())
+    option, index = pick(list(versionsNames), "Select a version", indicator='=>')
+    if option:
+        VERSION_DATA = versions.json()[index]
 
-if option:
-    downloadPath = versions.json()[index]['assets'][0]['browser_download_url']
-    r = requests.get(downloadPath)
+if VERSION_DATA.get('assets'):
+    openstackUrl = VERSION_DATA['assets'][0]['browser_download_url']
+    package = requests.get(openstackUrl)
 
-    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z = zipfile.ZipFile(io.BytesIO(package.content))
     z.extractall("dashboards/vprotect/static/vprotect")
     z.extractall("/usr/share/openstack-dashboard/static/vprotect")
 
@@ -53,3 +66,4 @@ if option:
 
     copy_tree('dashboards/vprotect/', '/usr/share/openstack-dashboard/openstack_dashboard/dashboards/vprotect/')
     shutil.copyfile('enabled/_50_vprotect.py', '/usr/share/openstack-dashboard/openstack_dashboard/enabled/_50_vprotect.py')
+
